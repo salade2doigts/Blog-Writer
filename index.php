d0e5f3d803cc7389307b8e434d8d29f9d5a6b762
Said LITIM <42178733+salitim@users.noreply.github.com>
2019-09-27 16:09:37 +0200

    indentation fix

--- index.php
+++ index.php
@@ -8,59 +8,52 @@ require('tabs.php');
 $controllerFront = new \Said\Projet4blog\controller\frontend\ControllerFront();
 $controllerBack = new \Said\Projet4blog\controller\backend\ControllerBack();    
 
-
-
-
 if (isset($_GET['action'])){
-
     foreach ($actionsFront as $action) {
         switch ($_GET['action']) {
             case $action:
-            return $controllerFront->$action();
-            break;      
+                return $controllerFront->$action();
+                break;      
             default:
-            http_response_code(404);
+                http_response_code(404);
         }
     }
 
     foreach ($actionFrontParams as $action => $value) {
-       switch ($_GET['action']) {
-        case $action:
-        return $controllerFront->$action(compact($value));
-        break;
-        default:
-        http_response_code(404);
-    }
-}       
+        switch ($_GET['action']) {
+            case $action:
+                return $controllerFront->$action(compact($value));
+                break;
+            default:
+                http_response_code(404);
+        }
+    }       
 
-foreach ($actionsBack as $action) {
-    switch ($_GET['action']) {
-        case $action:
-        return $controllerBack->$action();
-        break; 
-        default:
-        http_response_code(404);
-    }
-}
-
-foreach ($actionsBackParams as $action => $value) {
-    switch ($_GET['action']) {
-        case $action:
-        return $controllerBack->$action(compact($value));
-        break;
-        default:
-        http_response_code(404);
+    foreach ($actionsBack as $action) {
+        switch ($_GET['action']) {
+            case $action:
+                return $controllerBack->$action();
+                break; 
+            default:
+                http_response_code(404);
+       }
     }
 
-}
+    foreach ($actionsBackParams as $action => $value) {
+        switch ($_GET['action']) {
+            case $action:
+                return $controllerBack->$action(compact($value));
+                break;
+            default:
+                http_response_code(404);
+        } 
+    } 
 
-if($_GET['action'] ==='disconnect'){
-
-    $_SESSION = array();
-    session_destroy();
-    header('location: index.php');
-}
-
+    if($_GET['action'] ==='disconnect'){
+        $_SESSION = array();
+        session_destroy();
+        header('location: index.php');
+    }
 }else{
     # code...
     $listPosts = $controllerFront->listPosts();
